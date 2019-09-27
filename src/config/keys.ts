750f25738f2faa8882071215ee0849aff9d33f68
Edwin Oliveira <39379560+EdwinOliveira@users.noreply.github.com>
2019-09-27 12:41:08 +0100

    Update keys.ts

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,3 +1,3 @@
 export default {
-    mongoURI: 'mongodb+srv://user:pass@kecluster-rdlfg.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://edwin:edwin1234@kecluster-rdlfg.mongodb.net/test?retryWrites=true&w=majority'
 }
